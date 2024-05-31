d2fae99fb462afc37b0f8f98213154feb5871237
iamagoofyperson <162369657+iamagoofyperson@users.noreply.github.com>
2024-05-31 09:24:09 -0400

    Update script.js

--- script.js
+++ script.js
@@ -1,14 +1,9 @@
 const form = document.getElementById('proxy-form');
 const urlInput = document.getElementById('url');
-const proxyContent = document.getElementById('proxy-content');
 
 form.addEventListener('submit', (e) => {
   e.preventDefault();
   const url = urlInput.value;
-  const iframe = document.createElement('iframe');
-  iframe.src = `https://www.google.com/about:blank?q=${encodeURIComponent(url)}`;
-  iframe.style.height = '100vh';
-  iframe.style.width = '100vw';
-  proxyContent.innerHTML = '';
-  proxyContent.appendChild(iframe);
+  const newTab = window.open('', '_blank');
+  newTab.location.href = `https://www.google.com/search?q=${encodeURIComponent(url)}`;
 });
